8de07563d54dbfebcd8cc87ef239602e2d40db05
Kevin Kidd <harrison.kevin@pm.me>
2024-09-29 08:06:29 -0400

    refactor(utils): update import statements and enhance AI client handling

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,4 @@
-import type { Config } from "./config";
-import { PROMPT } from "./constants";
+import { COMMIT_MESSAGE_SCHEMA, PROMPT, SYSTEM_PROMPT } from "./constants";
 import OpenAI from "openai";
 import Anthropic from "@anthropic-ai/sdk";
 import Groq from "groq-sdk";
@@ -7,7 +6,9 @@ import type { ChatCompletionMessageParam as OpenAIMessageParam } from "openai/re
 import type { ChatCompletionMessageParam as GroqMessageParam } from "groq-sdk/resources/chat/completions.mjs";
 import type { MessageParam } from "@anthropic-ai/sdk/resources/messages.mjs";
 import { $ } from "bun";
-import simpleGit from "simple-git";
+import simpleGit, { type DefaultLogFields } from "simple-git";
+import * as v from "valibot";
+import type { AIClient, Config } from "./types";
 
 // Create client based on config AI provider
 export function createClient(
@@ -33,49 +34,73 @@ export function createClient(
 
 // Generate commit messages given client, amount, and git diff
 export async function generateCommitMessages(
-	client: OpenAI | Anthropic | Groq,
+	client: AIClient,
 	model: Config["model"],
 	maxTokens: Config["maxTokens"],
 	amount: number,
 	diff: string,
-) {
-	const prompt = PROMPT.replace("{{amount}}", amount.toString()).replace(
-		"{{diff}}",
-		diff,
-	);
+	recentCommits: string,
+): Promise<string[]> {
+	const prompt = PROMPT.replaceAll("{{amount}}", amount.toString())
+		.replaceAll("{{diff}}", diff)
+		.replaceAll("{{recent-commits}}", recentCommits);
+	let commitMessages: string | null | undefined;
 	const messages: OpenAIMessageParam[] | MessageParam[] | GroqMessageParam[] = [
+		{ role: "system", content: SYSTEM_PROMPT },
 		{ role: "user", content: prompt },
 	];
-	switch (client.constructor.name) {
-		case "OpenAI": {
-			const openaiClient = client as OpenAI;
-			const response = await openaiClient.chat.completions.create({
-				model,
-				max_tokens: maxTokens,
-				messages: messages as OpenAIMessageParam[],
-			});
-			return response.choices[0].message.content;
+	try {
+		switch (client.constructor.name) {
+			case "OpenAI": {
+				const openaiClient = client as OpenAI;
+				const response = await openaiClient.chat.completions.create({
+					model,
+					max_tokens: maxTokens,
+					messages: messages as OpenAIMessageParam[],
+				});
+				commitMessages = response.choices[0].message.content;
+				break;
+			}
+			case "Anthropic": {
+				const anthropicClient = client as Anthropic;
+				const response = await anthropicClient.messages.create({
+					model,
+					max_tokens: maxTokens,
+					messages: messages as MessageParam[],
+				});
+				commitMessages =
+					response.content[0].type === "text" ? response.content[0].text : null;
+				break;
+			}
+			case "Groq": {
+				const groqClient = client as Groq;
+				const response = await groqClient.chat.completions.create({
+					model,
+					max_tokens: maxTokens,
+					messages: messages as GroqMessageParam[],
+				});
+				commitMessages = response.choices[0].message.content;
+				break;
+			}
+			default:
+				throw new Error(`Unsupported provider: ${client.constructor.name}`);
 		}
-		case "Anthropic": {
-			const anthropicClient = client as Anthropic;
-			const response = await anthropicClient.messages.create({
-				model,
-				max_tokens: maxTokens,
-				messages: messages as MessageParam[],
-			});
-			return response;
+
+		if (!commitMessages) {
+			throw new Error("No commit messages generated");
 		}
-		case "Groq": {
-			const groqClient = client as Groq;
-			const response = await groqClient.chat.completions.create({
-				model,
-				max_tokens: maxTokens,
-				messages: messages as GroqMessageParam[],
-			});
-			return response.choices[0].message.content;
+
+		const jsonMatch = commitMessages.match(/{[\s\S]*}/);
+		const jsonResponse = jsonMatch ? jsonMatch[0] : null;
+		if (!jsonResponse) {
+			throw new Error("No valid JSON found in AI response");
 		}
-		default:
-			throw new Error(`Unsupported provider: ${client.constructor.name}`);
+		const parsedJson = JSON.parse(jsonResponse);
+		const validatedJson = v.parse(COMMIT_MESSAGE_SCHEMA, parsedJson);
+		return validatedJson.commitMessages;
+	} catch (error) {
+		console.error("Error generating commit messages:", error);
+		throw error;
 	}
 }
 
@@ -88,4 +113,17 @@ export async function getDiff() {
 		throw new Error("No staged changes found.");
 	}
 	return diff;
+}
+
+// Get recent commit messages
+export async function getRecentCommits() {
+	const currentDirectory = (await $`pwd`.text()).trim();
+	const git = simpleGit(currentDirectory);
+	const recentCommits = await git.log({
+		format: "%h %s",
+		maxCount: 10,
+	});
+	return recentCommits.all
+		.map((commit) => (commit as unknown as DefaultLogFields).message)
+		.join("\n");
 }
